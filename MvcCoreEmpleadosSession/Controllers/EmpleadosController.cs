a648d5e5c72364a14a2a6da079269b00941797b6
serraguti <pacoserranox@gmail.com>
2023-03-09 11:46:18 +0100

    Add project files.

---
+++ MvcCoreEmpleadosSession/Controllers/EmpleadosController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreEmpleadosSession.Models;
+using MvcCoreEmpleadosSession.Repositories;
+
+namespace MvcCoreEmpleadosSession.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private RepositoryEmpleados repo;
+
+        public EmpleadosController(RepositoryEmpleados repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult SessionSalarios()
+        {
+            List<Empleado> empleados = this.repo.GetEmpleados();
+            return View(empleados);
+        }
+
+        public IActionResult SumaSalarios()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcCoreEmpleadosSession/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,26 @@
+﻿using MvcCoreEmpleadosSession.Data;
+using MvcCoreEmpleadosSession.Models;
+
+namespace MvcCoreEmpleadosSession.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private EmpleadosContext context;
+
+        public RepositoryEmpleados(EmpleadosContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Empleado> GetEmpleados()
+        {
+            return this.context.Empleados.ToList();
+        }
+
+        public Empleado FindEmpleado(int idempleado)
+        {
+            return
+                this.context.Empleados.FirstOrDefault(x => x.IdEmpleado == idempleado);
+        }
+    }
+}
